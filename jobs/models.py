4d7fe78aec1d6447d11d45400de2753e0ba93ba8
trishank96 <mukherjeetrish005@gmail.com>
2019-08-12 11:26:56 +0530

    Added apps models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    images = models.ImageField(upload_to='media/')
+    summary = models.CharField(max_length=200)
